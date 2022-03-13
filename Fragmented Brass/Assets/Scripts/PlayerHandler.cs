bb4e1781f9ad7b7bbac41eeafb96ec654c8d8e10
Malachi <the.sentry.guy.sl@gmail.com>
2022-03-13 17:14:25 +0100

    GIT LFS

--- Fragmented Brass/Assets/Scripts/PlayerHandler.cs
+++ Fragmented Brass/Assets/Scripts/PlayerHandler.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHandler : MonoBehaviour,IHealthSys
+public class PlayerHandler : MonoBehaviour, IHealthSys
 {
     int maxHealth = 100;
     int currentHealth;
@@ -13,19 +13,13 @@ public class PlayerHandler : MonoBehaviour,IHealthSys
     // Start is called before the first frame update
     void Start()
     {
-        currentHealth=maxHealth;
-        Debug.Log("Health: "+currentHealth); 
+        currentHealth = maxHealth;
+        Debug.Log("Health: " + currentHealth);
         healthBar.SetMaxValue(maxHealth);
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.G))
-        {
-           Damage(10);
-           Debug.Log("Health: "+currentHealth);
-           healthBar.SetValue(currentHealth);
-        }
         if (Input.GetKeyDown(KeyCode.J))
         {
             isBuilding = !isBuilding;
@@ -35,21 +29,21 @@ public class PlayerHandler : MonoBehaviour,IHealthSys
     public void Damage(int dmg)
     {
         currentHealth -= dmg;
-        if(currentHealth <= 0) Die();
+        if (currentHealth <= 0) Die();
     }
 
     public void Heal(int heal)
     {
-        if(isAlive)
+        if (isAlive)
         {
             currentHealth += heal;
-            if(currentHealth>maxHealth) currentHealth=maxHealth;
+            if (currentHealth > maxHealth) currentHealth = maxHealth;
         }
     }
 
     public void Die()
     {
         Debug.Log("Player Died");
-        isAlive=false;
+        isAlive = false;
     }
 }
--- Fragmented Brass/Assets/Scripts/Recoil.cs
+++ Fragmented Brass/Assets/Scripts/Recoil.cs
@@ -2,10 +2,8 @@ using UnityEngine;
 
 public class Recoil : MonoBehaviour
 {
-    //Scripts
     [SerializeField] private Weapon Weapon_Script;
 
-    //Rotations
     private Vector3 currentRotation;
     private Vector3 targetRotation;
 
@@ -18,7 +16,7 @@ public class Recoil : MonoBehaviour
 
     public void RecoilFire()
     {
-        if(Weapon_Script.isAiming) targetRotation += new Vector3(Weapon_Script.aimRecoilX, Random.Range(-Weapon_Script.aimRecoilY, Weapon_Script.aimRecoilY), Random.Range(-Weapon_Script.aimRecoilZ, Weapon_Script.aimRecoilZ));
+        if (Weapon_Script.isAiming) targetRotation += new Vector3(Weapon_Script.aimRecoilX, Random.Range(-Weapon_Script.aimRecoilY, Weapon_Script.aimRecoilY), Random.Range(-Weapon_Script.aimRecoilZ, Weapon_Script.aimRecoilZ));
         else targetRotation += new Vector3(Weapon_Script.recoilX, Random.Range(-Weapon_Script.recoilY, Weapon_Script.recoilY), Random.Range(-Weapon_Script.recoilZ, Weapon_Script.recoilZ));
     }
 }
